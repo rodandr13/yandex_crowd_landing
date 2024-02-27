71ec5c985ed47f06c1d9fa86a40ad58aed9ed3b5
Andrey Roditelev <rodandr13@gmail.com>
2024-02-27 03:40:33 +0300

    refoctor: refactoring slideshow script

--- utils/slideShowSteps.js
+++ utils/slideShowSteps.js
@@ -1,119 +1,166 @@
-const gridSteps = document.querySelector('.grid-steps');
-const steps = document.querySelectorAll('.grid-steps__step');
-const prevButton = document.querySelector('.slider-controls__button_type_prev');
-const nextButton = document.querySelector('.slider-controls__button_type_next');
-let currentSlideIndex = 0;
-let cachedSlides = calculateSlides();
+function createSlider(gridStepsSelector, prevButtonSelector, nextButtonSelector) {
+  let currentSlideIndex = 0;
+  let gridSteps = document.querySelector(gridStepsSelector);
+  const steps = gridSteps.querySelectorAll('.grid-steps__step');
+  const prevButton = document.querySelector(prevButtonSelector);
+  const nextButton = document.querySelector(nextButtonSelector);
+  const indicatorsContainer = document.querySelector('.slider-controls__buttons-container');
+  let cachedSlides = calculateSlides(steps);
 
-function updateActiveIndicators() {
-  const indicators = document.querySelectorAll('.slider-controls__circle-button');
+  // Отображает активный слайд, обновляет состояние кнопок навигации
+  function showCurrentSlide() {
 
-  indicators.forEach((indicator, index) => {
-    if (index === currentSlideIndex) {
-      indicator.classList.add('slider-controls__circle-button_active');
-    } else {
-      indicator.classList.remove('slider-controls__circle-button_active');
-    }
-  })
-}
+    steps.forEach((step) => {
+      step.style.visibility = 'hidden';
+      step.style.position = 'absolute';
+    });
 
-function addIndicators() {
-  const indicatorContainer = document.querySelector('.slider-controls__buttons-container');
-  const slides = cachedSlides;
-  indicatorContainer.innerHTML = '';
+    cachedSlides[currentSlideIndex].forEach((step) => {
+      step.style.visibility = 'visible';
+      step.style.position = 'relative';
+    });
 
-  for (let i = 0; i < slides.length; i++) {
-    const indicator = document.createElement('button');
-    indicator.classList.add('slider-controls__circle-button');
-    indicator.addEventListener('click', () => {
-      currentSlideIndex = i;
-      showCurrentSlide();
-    })
-    indicatorContainer.appendChild(indicator);
+    updateActiveIndicators();
+
+    prevButton.disabled = currentSlideIndex === 0;
+    nextButton.disabled = currentSlideIndex === cachedSlides.length - 1;
   }
 
-  updateActiveIndicators();
-}
+  // Рассчитывает и группирует шаги в слайды, в зависимости от высоты шагов с учетом контента.
+  function calculateSlides() {
+    const gridStepsStyles = window.getComputedStyle(gridSteps);
+    const gridStepsPaddingTop = parseInt(gridStepsStyles.paddingTop, 10);
+    const gridStepsPaddingBottom = parseInt(gridStepsStyles.paddingBottom, 10);
+    const containerHeight = gridSteps.offsetHeight - gridStepsPaddingBottom - gridStepsPaddingTop;
 
-function showCurrentSlide() {
-  const slides = cachedSlides;
-  steps.forEach((step) => {
-    step.style.visibility = 'hidden';
-    step.style.position = 'absolute';
-  })
-  slides[currentSlideIndex].forEach((step) => {
-    step.style.visibility = 'visible';
-    step.style.position = 'relative';
-  })
-  updateActiveIndicators();
+    const slides = [];
+    let currentSlide = [];
+    let totalHeight = 0;
 
-  prevButton.disabled = currentSlideIndex === 0;
-  nextButton.disabled = currentSlideIndex === slides.length - 1;
-}
-
-
-nextButton.addEventListener('click', () => {
-  console.log('click next');
-  if (currentSlideIndex < cachedSlides.length - 1) {
-    currentSlideIndex++;
-    showCurrentSlide();
-  }
-})
-
-prevButton.addEventListener('click', () => {
-  console.log('click prev');
-  if (currentSlideIndex > 0) {
-    currentSlideIndex--;
-    showCurrentSlide();
-  }
-})
-addIndicators();
-showCurrentSlide();
-
-function calculateSlides() {
-  const gridStepsStyles = window.getComputedStyle(gridSteps);
-  const gridStepsPaddingTop = parseInt(gridStepsStyles.paddingTop, 10);
-  const gridStepsPaddingBottom = parseInt(gridStepsStyles.paddingBottom, 10);
-  const containerHeight = gridSteps.offsetHeight - gridStepsPaddingBottom - gridStepsPaddingTop;
-
-  const slides = [];
-  let currentSlide = [];
-
-  let totalHeight = 0;
-
-  steps.forEach((step) => {
-    const stepHeight = step.offsetHeight;
-    if (stepHeight > containerHeight / 2) {
-      if (totalHeight > 0) {
+    steps.forEach((step) => {
+      const stepHeight = step.offsetHeight;
+      if (stepHeight > containerHeight / 2) {
+        if (totalHeight > 0) {
+          slides.push(currentSlide);
+          currentSlide = [];
+          totalHeight = 0;
+        }
+        slides.push([step]);
+      } else if (totalHeight + stepHeight > containerHeight) {
         slides.push(currentSlide);
-        currentSlide = [];
-        totalHeight = 0;
+        currentSlide = [step];
+        totalHeight = stepHeight;
+      } else {
+        currentSlide.push(step);
+        totalHeight += stepHeight;
       }
-      slides.push([step]);
-    } else if (totalHeight + stepHeight > containerHeight) {
+    });
+
+    if (currentSlide.length > 0) {
       slides.push(currentSlide);
-      currentSlide = [step];
-      totalHeight = stepHeight;
-    } else {
-      currentSlide.push(step);
-      totalHeight += stepHeight;
     }
-  });
 
-  if (currentSlide.length > 0) {
-    slides.push(currentSlide);
+    return slides;
   }
-  return slides;
+
+  // Добавляет индикаторы для каждого слайда
+  function addIndicators() {
+    const indicatorContainer = document.querySelector('.slider-controls__buttons-container');
+    indicatorContainer.innerHTML = '';
+
+    for (let i = 0; i < cachedSlides.length; i++) {
+      const indicator = document.createElement('button');
+      indicator.classList.add('slider-controls__circle-button');
+      indicator.addEventListener('click', () => {
+        currentSlideIndex = i;
+        showCurrentSlide();
+      })
+
+      indicatorContainer.appendChild(indicator);
+    }
+
+    updateActiveIndicators();
+  }
+
+  // Обновляет активный индикатор слайда
+  function updateActiveIndicators() {
+    indicatorsContainer.querySelectorAll('.slider-controls__circle-button').forEach((indicator, index) => {
+      indicator.classList.toggle('slider-controls__circle-button_active', index === currentSlideIndex);
+    });
+  }
+
+  let isSliderInitialized = false;
+  let eventListenersAdded = false;
+
+  // Инициализиует слайдер, добавляет слушатели кнопок навигации
+  function initSlider() {
+    if (!isSliderInitialized) {
+      cachedSlides = calculateSlides();
+      currentSlideIndex = Math.min(currentSlideIndex, cachedSlides.length - 1);
+      addIndicators();
+      showCurrentSlide();
+
+      if (!eventListenersAdded) {
+        nextButton.addEventListener('click', nextSlide);
+        prevButton.addEventListener('click', prevSlide);
+        eventListenersAdded = true;
+      }
+      isSliderInitialized = true;
+    }
+  }
+
+  // Удаляет слушатели кнопок и возвращает компонент к исходному виду
+  function deinitSlider() {
+    if (isSliderInitialized) {
+      steps.forEach(step => {
+        step.style.visibility = '';
+        step.style.position = '';
+      });
+      if (eventListenersAdded) {
+        nextButton.removeEventListener('click', nextSlide);
+        prevButton.removeEventListener('click', prevSlide);
+        eventListenersAdded = false;
+      }
+      isSliderInitialized = false;
+    }
+  }
+
+  // Следующий слайд
+  function nextSlide() {
+    if (currentSlideIndex < cachedSlides.length - 1) {
+      currentSlideIndex++;
+      showCurrentSlide();
+    }
+  }
+
+  // Предыдущий слайд
+  function prevSlide() {
+    if (currentSlideIndex > 0) {
+      currentSlideIndex--;
+      showCurrentSlide();
+    }
+  }
+
+  // Проверяет инициализацию слайдера для ширены менее 680px
+  function checkSlideShow() {
+    if (window.matchMedia('(max-width: 680px)').matches) {
+      if (!isSliderInitialized) {
+        initSlider();
+      } else {
+        cachedSlides = calculateSlides(steps);
+        addIndicators();
+        showCurrentSlide();
+      }
+    } else {
+      deinitSlider();
+    }
+  }
+
+  checkSlideShow();
+  window.addEventListener('resize', checkSlideShow);
+
 }
 
-function updateSlidesAndIndicators() {
-  cachedSlides = calculateSlides();
-  currentSlideIndex = Math.min(currentSlideIndex, cachedSlides.length - 1);
-  addIndicators();
-  showCurrentSlide();
-}
-
-
-
-window.addEventListener('resize', updateSlidesAndIndicators);
-
+document.addEventListener('DOMContentLoaded', function () {
+  createSlider('.grid-steps', '.slider-controls__button_type_prev', '.slider-controls__button_type_next');
+});
